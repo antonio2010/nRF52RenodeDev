6646dbffd7d3be31916571298bea455ddeb1ddaa
antonio2010 <samuelantonio83@gmail.com>
2021-08-30 03:51:14 -0400

    Initial commit

---
+++ zephyr/nrf52840dk_nrf52840.dts.pre.d
@@ -0,0 +1,11 @@
+empty_file.o: C:/Users/anton/ncs/v1.6.1/zephyr/misc/empty_file.c \
+ C:/Users/anton/ncs/v1.6.1/zephyr/boards/arm/nrf52840dk_nrf52840/nrf52840dk_nrf52840.dts \
+ C:/Users/anton/ncs/v1.6.1/zephyr/dts/arm/nordic/nrf52840_qiaa.dtsi \
+ C:/Users/anton/ncs/v1.6.1/zephyr/dts/common/mem.h \
+ C:/Users/anton/ncs/v1.6.1/zephyr/dts/arm/nordic/nrf52840.dtsi \
+ C:/Users/anton/ncs/v1.6.1/zephyr/dts/arm/armv7-m.dtsi \
+ C:/Users/anton/ncs/v1.6.1/zephyr/dts/common/skeleton.dtsi \
+ C:/Users/anton/ncs/v1.6.1/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ C:/Users/anton/ncs/v1.6.1/zephyr/include/dt-bindings/i2c/i2c.h \
+ C:/Users/anton/ncs/v1.6.1/zephyr/include/dt-bindings/gpio/gpio.h \
+ C:/Users/anton/ncs/v1.6.1/zephyr/dts/arm/nordic/override.dtsi
